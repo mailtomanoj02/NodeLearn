0fe210b059f07c2f4236a2bcbe72b05250e0ca51
manoj.s@foodhub.com <manoj.s@foodhub.com>
2024-05-25 11:09:32 +0530

    body-parser

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const routes = require("./routes");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false })); // inbuilt have next() in it
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send("<h1>Hello from Add Product</h1>"); // return this response only for add-product
+  res.send(
+    '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  ); // return this response only for add-product
   // next();
 });
+app.use("/product", (req, res, next) => {
+  console.log("In the middleware 2==>", req.body);
+  res.redirect("/");
+});
+
 app.use((req, res, next) => {
   console.log("In the middleware 2");
   res.send("<h1>Hello from Express</h1>");
