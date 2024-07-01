35c8c93b553f0cccacf001db26a98181c44167a2
manoj.s@foodhub.com <manoj.s@foodhub.com>
2024-07-01 19:42:41 +0530

    04-tempating-engine

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const routes = require("./routes");
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false })); // inbuilt have next() in it
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  ); // return this response only for add-product
-  // next();
-});
-app.use("/product", (req, res, next) => {
-  console.log("In the middleware 2==>", req.body);
-  res.redirect("/");
-});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In the middleware 2");
-  res.send("<h1>Hello from Express</h1>");
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-//64
